895be8328756cd9b14395538b77e83e6d2133e4d
David Abreu <david.abreu@mpdft.mp.br>
2022-07-20 08:55:44 -0300

    UserRepository - n+1 queries solved join fetch user roles

--- src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
@@ -2,9 +2,13 @@ package com.devsuperior.movieflix.repositories;
 
 import com.devsuperior.movieflix.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("select u from User u " +
+            "JOIN FETCH u.roles " +
+            "where u.email = :username")
     Optional<User> findByEmail(String username);
 }
--- src/main/java/com/devsuperior/movieflix/service/UserService.java
+++ src/main/java/com/devsuperior/movieflix/service/UserService.java
@@ -1,11 +1,9 @@
 package com.devsuperior.movieflix.service;
 
 import com.devsuperior.movieflix.dto.UserDto;
-import com.devsuperior.movieflix.entities.Role;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.exceptions.ResourceNotFoundException;
 import com.devsuperior.movieflix.exceptions.UnauthorizedException;
-import com.devsuperior.movieflix.repositories.RoleRepository;
 import com.devsuperior.movieflix.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,20 +11,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    private final RoleRepository roleRepository;
-
     private final AuthService authService;
 
-    public UserService(UserRepository userRepository, RoleRepository roleRepository, AuthService authService) {
+    public UserService(UserRepository userRepository, AuthService authService) {
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
         this.authService = authService;
     }
 
@@ -53,20 +46,8 @@ public class UserService implements UserDetailsService {
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username)  {
-        User user =  userRepository.findByEmail(username)
+        return userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-        initRoles(user);
-
-        return user;
-    }
-
-    private void initRoles(User user) {
-        List<Role> roles = user.getRoles().stream()
-                .map(role -> roleRepository.findById(role.getId())
-                        .orElseThrow(() -> new RuntimeException("Role not found")))
-                .toList();
-        user.getRoles().clear();
-        user.getRoles().addAll(roles);
     }
 
     public UserDto profile() {
